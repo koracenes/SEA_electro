3d9aaba7a6d4bc513aae06cd3b9b23c73a540e2e
Enes Korac <korac.enes2@gmail.com>
2025-05-09 11:08:46 +0200

    headera and nav change

--- responsive-construction-website-main/assets/js/main.js
+++ responsive-construction-website-main/assets/js/main.js
@@ -28,6 +28,15 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () => {
+  const header = document.getElementById("header");
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", bgHeader);
+bgHeader();
 
 /*=============== SWIPER SERVICES ===============*/
 
