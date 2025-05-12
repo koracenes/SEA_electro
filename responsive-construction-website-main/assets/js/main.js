2ef88b1516d1bd0aa4825659ee5bb9ad577198ec
Enes Korac <korac.enes2@gmail.com>
2025-05-12 09:23:32 +0200

    projects

--- responsive-construction-website-main/assets/js/main.js
+++ responsive-construction-website-main/assets/js/main.js
@@ -39,6 +39,16 @@ window.addEventListener("scroll", bgHeader);
 bgHeader();
 
 /*=============== SWIPER SERVICES ===============*/
+const swiperServices = new Swiper(".services__swiper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 24,
+  slidesPerView: "auto",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 /*=============== SHOW SCROLL UP ===============*/
 
