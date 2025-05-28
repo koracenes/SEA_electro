ab4a71cfd310bfaee0f39a303ada5c224d9ff1fa
Enes Korac <korac.enes2@gmail.com>
2025-05-28 12:56:17 +0200

    some fixes

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -13,5 +13,37 @@ search.addEventListener("click", () => {
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
   )
     .then((response) => response.json())
-    .then((json) => {});
+    .then((json) => {
+      const image = document.querySelector(".weather-box img");
+      const temperature = document.querySelector(".weather-box .temperature");
+      const description = document.querySelector(".weather-box .description");
+      const humidity = document.querySelector(
+        ".weather-details .humidity span"
+      );
+      const wind = document.querySelector(".weather-details .wind span");
+
+      switch (json.weather[0].main) {
+        case "Clear":
+          image.src = "images/clear.png";
+          break;
+        case "Rain":
+          image.src = "images/rain.png";
+          break;
+        case "Snow":
+          image.src = "images/snow.png";
+          break;
+        case "Clouds":
+          image.src = "images/cloud.png";
+          break;
+        case "Mist":
+          image.src = "images/mist.png";
+          break;
+        case "Haze":
+          image.src = "images/mist.png";
+          break;
+        default:
+          image.src = "images/cloud.png";
+          break;
+      }
+    });
 });
